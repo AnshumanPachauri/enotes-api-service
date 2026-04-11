dafd01f77fa1acfee1af59da66e41627a0867b1c
Anshuman <anshumanpachauri26@gmail.com>
2026-04-11 23:58:15 +0530

    Implemented User Controller.

---
+++ src/main/java/com/ap/enotes_api_service/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.ap.enotes_api_service.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ap.enotes_api_service.dto.UserDto;
+import com.ap.enotes_api_service.service.UserService;
+import com.ap.enotes_api_service.utils.CommonUtil;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class AuthController {
+
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/")
+	public ResponseEntity<?> registerUser(@RequestBody UserDto userDto){
+		
+		Boolean register = userService.register(userDto);
+		
+		if(register) {
+			return CommonUtil.CreateBuildResponseMessage("user registered successfully", HttpStatus.CREATED);
+		}
+		return CommonUtil.CreateErrorResponseMessage("User Not registered.", HttpStatus.INTERNAL_SERVER_ERROR);
+		
+	}
+	
+}
