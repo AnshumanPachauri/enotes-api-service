b309145dbf3b3a333397ae5bde3d818508d11e68
Anshuman <anshumanpachauri26@gmail.com>
2026-04-24 20:48:22 +0530

    Created UserResponseDto for the Response given to User. added an inner class i.e. statusDto in the userResponseDto.

---
+++ src/main/java/com/ap/enotes_api_service/dto/UserResponseDto.java
@@ -0,0 +1,51 @@
+package com.ap.enotes_api_service.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class UserResponseDto {
+
+
+	private Integer id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
+	
+//	private String password;
+	
+	private String mobileNumber;
+	
+	private StatusDto status;
+
+	private List<RoleDto> roles;
+	
+	@Data
+	@AllArgsConstructor
+	@NoArgsConstructor
+	@Builder
+	public static class RoleDto{
+		
+		private Integer id;
+		private String name;
+		
+	}
+	
+	@Data
+	@AllArgsConstructor
+	@NoArgsConstructor
+	@Builder
+	public static class StatusDto{
+		
+		private int id;
+		private Boolean isActive;
+		
+	}
+	
+}
