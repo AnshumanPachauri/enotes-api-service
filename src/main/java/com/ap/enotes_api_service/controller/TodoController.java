d8211cf56918beb435e3f72e48960f7b5a28c0a8
Anshuman <anshumanpachauri26@gmail.com>
2026-07-04 20:20:54 +0530

    Implemented endpoint abstraction in TodoController.

--- src/main/java/com/ap/enotes_api_service/controller/TodoController.java
+++ src/main/java/com/ap/enotes_api_service/controller/TodoController.java
@@ -16,18 +16,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ap.enotes_api_service.dto.TodoDto;
+import com.ap.enotes_api_service.endpoints.TodoControllerEndpoint;
 import com.ap.enotes_api_service.service.TodoService;
 import com.ap.enotes_api_service.utils.CommonUtil;
 
 @RestController
 @RequestMapping("/api/v1/todo")
-public class TodoController {
+public class TodoController implements TodoControllerEndpoint{
 
 	@Autowired
 	private TodoService todoService;
 	
-	@PostMapping("/")
-	@PreAuthorize("hasRole('USER')")
+//	@PostMapping("/")
+//	@PreAuthorize("hasRole('USER')")
+	@Override
 	public ResponseEntity<?> saveTodo(@RequestBody TodoDto todoDto) throws Exception{
 		
 		Boolean saveTodo = todoService.saveTodo(todoDto);
@@ -37,16 +39,18 @@ public class TodoController {
 		return CommonUtil.CreateErrorResponseMessage("Todo Not Saved", HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
-	@GetMapping("/{id}")
-	@PreAuthorize("hasRole('USER')")
+//	@GetMapping("/{id}")
+//	@PreAuthorize("hasRole('USER')")
+	@Override
 	public ResponseEntity<?> getTodoById(@PathVariable Integer id) throws Exception{
 		
 		TodoDto todoById = todoService.getTodoByid(id);
 		return CommonUtil.CreateBuildResponse(todoById, HttpStatus.OK);
 	}
 	
-	@GetMapping("/user-todo")
-	@PreAuthorize("hasRole('USER')")
+//	@GetMapping("/user-todo")
+//	@PreAuthorize("hasRole('USER')")
+	@Override
 	public ResponseEntity<?> getTodoByUser() throws Exception{
 		
 		List<TodoDto> todosByUser = todoService.getTodoByUser();
