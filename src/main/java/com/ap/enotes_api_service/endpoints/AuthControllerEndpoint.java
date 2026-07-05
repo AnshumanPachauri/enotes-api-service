ca7c5e3e68d4466e9a73c02d345ebf8249ea384f
Anshuman <anshumanpachauri26@gmail.com>
2026-07-05 13:51:58 +0530

    Added swagger configuration to Application.yml for path, operation and tags sorter.

--- src/main/java/com/ap/enotes_api_service/endpoints/AuthControllerEndpoint.java
+++ src/main/java/com/ap/enotes_api_service/endpoints/AuthControllerEndpoint.java
@@ -11,14 +11,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 
-@Tag(name="User Authentication", description = "Contains all the user authentication api's")
+@Tag(name="Authentication", description = "Contains all the user authentication api's")
 public interface AuthControllerEndpoint {
 
-	@Operation(summary = "User Registeration Endpoint", tags= {"User Authentication", "Home"})
+	@Operation(summary = "User Registeration Endpoint", tags= {"Authentication", "Home"})
 	@PostMapping("/register")
 	public ResponseEntity<?> registerUser(@RequestBody UserRequestDto userDto, HttpServletRequest request) throws Exception;
 	
-	@Operation(summary = "User Login Endpoint", tags= {"User Authentication", "Home"})
+	@Operation(summary = "User Login Endpoint", tags= {"Authentication", "Home"})
 	@PostMapping("/login")
 	public ResponseEntity<?> login(@RequestBody LoginRequestDto loginRequestDto) throws Exception;
 	
