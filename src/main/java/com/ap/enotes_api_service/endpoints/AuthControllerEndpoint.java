c26bc261f80567fb92c042cb391cd919d9880974
Anshuman <anshumanpachauri26@gmail.com>
2026-07-05 13:02:16 +0530

    Added swagger configuration to auth and category controller.

--- src/main/java/com/ap/enotes_api_service/endpoints/AuthControllerEndpoint.java
+++ src/main/java/com/ap/enotes_api_service/endpoints/AuthControllerEndpoint.java
@@ -7,14 +7,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.ap.enotes_api_service.dto.LoginRequestDto;
 import com.ap.enotes_api_service.dto.UserRequestDto;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 
+@Tag(name="User Authentication", description = "Contains all the user authentication api's")
 public interface AuthControllerEndpoint {
 
-	
+	@Operation(summary = "User Registeration Endpoint", tags= {})
 	@PostMapping("/register")
 	public ResponseEntity<?> registerUser(@RequestBody UserRequestDto userDto, HttpServletRequest request) throws Exception;
 	
+	@Operation(summary = "User Login Endpoint", tags= {})
 	@PostMapping("/login")
 	public ResponseEntity<?> login(@RequestBody LoginRequestDto loginRequestDto) throws Exception;
 	
--- src/main/java/com/ap/enotes_api_service/endpoints/CategoryControllerEndpoint.java
+++ src/main/java/com/ap/enotes_api_service/endpoints/CategoryControllerEndpoint.java
@@ -10,30 +10,39 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import com.ap.enotes_api_service.dto.CategoryDto;
 import com.ap.enotes_api_service.utils.Constants;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import static com.ap.enotes_api_service.utils.Constants.ROLE_ADMIN;
 import static com.ap.enotes_api_service.utils.Constants.ROLE_ADMIN_USER;
 
 import jakarta.validation.Valid;
 
+@Tag(name="Category", description = "Contains all the notes category api's")
 public interface CategoryControllerEndpoint {
 
-	
+	@Operation(summary = "Save Notes Category", tags= {}, description = "Admin can save notes category")
 	@PostMapping("/save")
 	@PreAuthorize(ROLE_ADMIN)
 	public ResponseEntity<?> saveCategory(@Valid @RequestBody CategoryDto categoryDto);
 	
+	@Operation(summary = "Get All Notes Category", tags= {}, description = "Admin can get all notes category")
 	@GetMapping("/")
 	@PreAuthorize(ROLE_ADMIN)
 	public ResponseEntity<?> getAllCategory();
 	
+	@Operation(summary = "Get Only Active Notes Category", tags= {}, description = "Admin and User can get active notes category")
 	@GetMapping("/active")
 	@PreAuthorize(ROLE_ADMIN_USER)
 	public ResponseEntity<?> getActiveCategory();
 	
+	@Operation(summary = "Get All Notes Category By Id", tags= {}, description = "Admin can get notes category by id")
 	@GetMapping("/{id}")
 	@PreAuthorize(ROLE_ADMIN)
 	public ResponseEntity<?> getCategoryDetailsById(@PathVariable Integer id) throws Exception;
 	
+	@Operation(summary = "Delete Notes Category By Id", tags= {}, description = "Admin can delete notes category by id")
 	@DeleteMapping("/{id}")
 	@PreAuthorize(ROLE_ADMIN)
 	public ResponseEntity<?> deleteCategoryDetailsById(@PathVariable Integer id);
