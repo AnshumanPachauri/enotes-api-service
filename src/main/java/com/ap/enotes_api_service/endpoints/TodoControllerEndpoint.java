c5a023ca7d9afd74bbe6c6e632e1cbd5ba261b22
Anshuman <anshumanpachauri26@gmail.com>
2026-07-05 13:31:14 +0530

    Added swagger configuration to Todo controller.

--- src/main/java/com/ap/enotes_api_service/endpoints/TodoControllerEndpoint.java
+++ src/main/java/com/ap/enotes_api_service/endpoints/TodoControllerEndpoint.java
@@ -8,19 +8,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.ap.enotes_api_service.dto.TodoDto;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import static com.ap.enotes_api_service.utils.Constants.ROLE_USER;
 
+@Tag(name="Todo", description = "Contains all the Todo Operation api's")
 public interface TodoControllerEndpoint {
 
 	
+	@Operation(summary = "Save Todo's", tags= {}, description = "User can save their todo tasks")
 	@PostMapping("/")
 	@PreAuthorize(ROLE_USER)
 	public ResponseEntity<?> saveTodo(@RequestBody TodoDto todoDto) throws Exception;
 	
+	
+	@Operation(summary = "Get Todo's By Todo-Id", tags= {}, description = "User can get their todo tasks by todo-id")
 	@GetMapping("/{id}")
 	@PreAuthorize(ROLE_USER)
 	public ResponseEntity<?> getTodoById(@PathVariable Integer id) throws Exception;
 	
+	
+	@Operation(summary = "Get Todo's By User", tags= {}, description = "User can get their todo tasks")
 	@GetMapping("/user-todo")
 	@PreAuthorize(ROLE_USER)
 	public ResponseEntity<?> getTodoByUser() throws Exception;
